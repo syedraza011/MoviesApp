ddbabc2b33289cc89ee1b6ea78347f8687d8f8a1
Syed Raza <syed.raza011@gmail.com>
2023-07-09 10:48:10 -0400

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Syed Raza on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
